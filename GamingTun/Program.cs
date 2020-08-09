44e874ce5e5b5741f1968b4449b0e521896d6f1e
七月 <qiy@ywsoftware.com>
2020-08-10 01:04:18 +0800

    add tunnel

--- GamingTun/Program.cs
+++ GamingTun/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GamingTun
@@ -10,17 +10,78 @@ namespace GamingTun
     {
         static void Main(string[] args)
         {
-            var config = new Config();
-            var adapter = new TapAdapter(config);
-            Console.CancelKeyPress += Console_CancelKeyPress;
-            adapter.Start();
-            Console.WriteLine("Program is running, press enter to exit");
-            Console.ReadLine();
-            Console.WriteLine("Shutting down...");
-            adapter.Stop();
+            var program = new Program();
+
+            program.Setup();
+
+            Task.Run(program.Tunnel).Wait();
+
+            program.Wait();
         }
 
-        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        private void Adapter()
+        {
+            var config1 = new Config()
+            {
+                AdapterName = "Tun1",
+                Local = IPAddress.Parse("192.168.46.10")
+            };
+            var adapter1 = new TapAdapter(config1);
+            var config2 = new Config()
+            {
+                AdapterName = "Tun2",
+                Local = IPAddress.Parse("192.168.46.20")
+            };
+            var adapter2 = new TapAdapter(config2);
+            adapter1.Start();
+            adapter2.Start();
+            adapter1.OtherAdapter = adapter2;
+            adapter2.OtherAdapter = adapter1;
+            Task.Run(adapter1.Run);
+            Task.Run(adapter2.Run);
+            Console.WriteLine("Program is running");
+            adapter1.Stop();
+            adapter2.Stop();
+        }
+
+        private async Task Tunnel()
+        {
+            using (Tunnel tun1 = new Tunnel(6000))
+            using (Tunnel tun2 = new Tunnel(6050))
+            {
+                var cancellationTokenSource = new CancellationTokenSource();
+                var task1 = tun1.Receive(cancellationTokenSource.Token);
+                var task2 = tun2.Receive(cancellationTokenSource.Token);
+
+                await tun1.Connect(new IPEndPoint(IPAddress.Parse("172.16.10.38"), 6050));
+
+                string input;
+
+                do
+                {
+                    input = Console.ReadLine();
+
+                    await tun1.Send(Encoding.Default.GetBytes(input));
+
+                } while (input != "exit");
+
+                cancellationTokenSource.Cancel();
+            }
+        }
+
+        private void Setup()
+        {
+            Console.CancelKeyPress += Console_CancelKeyPress;
+        }
+
+        private void Wait()
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+            Console.WriteLine("Shutting down...");
+        }
+
+        private void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
         {
             e.Cancel = true;
         }
---
+++ GamingTun/Tunnel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GamingTun
+{
+    public class Tunnel : IDisposable
+    {
+        private readonly TapAdapter adapter;
+        private readonly UdpClient client;
+
+        public Tunnel(int port)
+        {
+            client = new UdpClient(port, AddressFamily.InterNetwork);
+        }
+
+        private volatile bool state;
+
+        public Task Connect(IPEndPoint endPoint)
+        {
+            client.Connect(endPoint);
+
+            state = true;
+
+            return Task.CompletedTask;
+        }
+
+        public async Task Receive(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var receiveResult = await client.ReceiveAsync();
+
+                if (!state)
+                {
+                    //await Connect(receiveResult.RemoteEndPoint);
+                }
+
+                var buffer = receiveResult.Buffer;
+
+                Console.WriteLine($"From {receiveResult.RemoteEndPoint.Port}: {Encoding.UTF8.GetString(buffer)}");
+            }
+        }
+
+        public async Task Send(byte[] buffer)
+        {
+            await client.SendAsync(buffer, buffer.Length);
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
